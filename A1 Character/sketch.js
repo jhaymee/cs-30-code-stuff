1163a84e3c281b0cfcefe3841ab8ab140f2ee780
jhaymee <jhaymee-belle.bisenio@rcsd.ca>
2024-02-29 12:09:53 -0600

    added p5js file, made something

---
+++ A1 Character/sketch.js
@@ -0,0 +1,19 @@
+// A1 Character
+// Jhaymee Bisenio
+// 29 Feb 2024
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  ellipse(100,100,100,100);
+  rect(55,180,100,200)
+  rect(55,400,40,200)
+  rect(115,400,40,200)
+}
