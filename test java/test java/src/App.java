3b9ae5d5481daa139d78e0e8aab8ce80bd3c5f1e
jhaymee <jhaymee-belle.bisenio@rcsd.ca>
2024-03-18 11:24:24 -0600

    java
    created java project

--- test java/test java/src/App.java
+++ test java/test java/src/App.java
@@ -1,6 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("My name is Jhaymee!");
+        System.out.println("\t tab hiiiiiiiiiiiiiiiiiiiiiiiiii \"HELLO\" \'stan loona\'");
     }
 }
