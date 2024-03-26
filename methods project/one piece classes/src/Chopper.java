14473ec390b1243b415c60247dc56ef1ce6cdfaa
jhaymee <jhaymee-belle.bisenio@rcsd.ca>
2024-03-26 12:06:24 -0600

    26 mar 24
    started classes project

---
+++ methods project/one piece classes/src/Chopper.java
@@ -0,0 +1,5 @@
+public class Chopper {
+    public void phrase() {
+        System.out.println("Somebody get a doctor! Oh wait--I'M the doctor!");
+    }
+}
---
+++ methods project/one piece classes/src/OnePieceRunner.java
@@ -0,0 +1,5 @@
+public class OnePieceRunner {
+    public static void main(String[] args) throws Exception {
+        
+    }
+}
