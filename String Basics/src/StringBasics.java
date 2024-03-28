e7b2c628000a420e97ca68ce89b0dda987c09faf
jhaymee <jhaymee-belle.bisenio@rcsd.ca>
2024-03-28 12:09:57 -0600

    worked on string basics

--- String Basics/src/StringBasics.java
+++ String Basics/src/StringBasics.java
@@ -1,5 +1,10 @@
-public class App {
+// Program name: String Basics
+// Student name: jhaymee Bisenio
+// Date: 28 March 2024
+// Program Description: manipulates strings
+
+public class StringBasics {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String message 1 = "I am very happy!";
     }
 }
