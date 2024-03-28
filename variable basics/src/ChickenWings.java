0fb03a8223ac88bb26cafd45615dbb41012a7210
jhaymee <jhaymee-belle.bisenio@rcsd.ca>
2024-03-28 11:41:38 -0600

    chicken wings

---
+++ variable basics/src/ChickenWings.java
@@ -0,0 +1,13 @@
+// Program name: Chicken Wings
+// Student name: Jhaymee Bisenio
+// Date: 28 March 2024
+// Program Description:
+
+public class ChickenWings {
+    public static void main(String[] args) throws Exception {
+        double oneWing = 0.75;
+        double userMoney = 4.75;
+        double howManyWings = 4.75 / 0.75;
+        System.out.println("You can buy " + (int)howManyWings + " wings.");
+    }
+}
