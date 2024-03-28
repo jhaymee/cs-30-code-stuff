d0bac75b594148b6098d244a9da526552447782e
jhaymee <jhaymee-belle.bisenio@rcsd.ca>
2024-03-28 12:09:39 -0600

    Update App.java

--- project/class practice/src/App.java
+++ project/class practice/src/App.java
@@ -5,7 +5,7 @@
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Welcome to Computer Science 30. \n My Name is Jhaymee and my favourite food is chicken wings.");
+        System.out.println("Welcome to Computer Science 30. \nMy Name is Jhaymee and my favourite food is chicken wings.");
         System.out.println("Java has all sorta of weird \"quirks\" which make displaying text a bit more challenging.");
         System.out.println("Sometimes characters like parentheses (sometimes called brackets) don't work correctly.");
         System.out.println("Escape sequences require us to use a backslash '\\' in order to output these special characters.");
