74d75dea273fa44534e850d5ca977fbe0dbd0783
jhaymee <jhaymee-belle.bisenio@rcsd.ca>
2024-04-10 11:19:27 -0600

    worked on methods

--- methods project/one piece classes/src/Luffy.java
+++ methods project/one piece classes/src/Luffy.java
@@ -2,4 +2,7 @@ public class Luffy {
     public void phrase() {
         System.out.println("I'm going to be King of the Pirates!");
     }
+    public void backstory() {
+        System.out.println("");
+    }
 }
--- methods project/one piece classes/src/OnePieceRunner.java
+++ methods project/one piece classes/src/OnePieceRunner.java
@@ -1,3 +1,11 @@
+// Program name: One Piece Classes
+
+// Student name: Jhaymee Bisenio
+
+// Date: 10 Apr 2024
+
+// Program Description: two one piece characters, luffy and chopper, both have phrase and backstory methods. phrase makes them say an iconic phrase from them, and the backstory tells you their backstory.
+
 public class OnePieceRunner {
     public static void main(String[] args) throws Exception {
         
