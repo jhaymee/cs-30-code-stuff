19a92dd360e34566f1dc5232ffe37a4759a6d38f
jhaymee <jhaymee-belle.bisenio@rcsd.ca>
2024-04-24 12:12:17 -0600

    started area of a triangle

---
+++ Area of a Triangle/src/Triangle.java
@@ -0,0 +1,57 @@
+// Program name: Area of a Triangle
+
+// Student name: Jhaymee Bisenio
+
+// Date: 24 April 2024
+
+// Program Description: calculates area of a triangle given the sides
+
+import java.util.Scanner; 
+import java.lang.Math.*;
+
+public class Triangle
+{
+	private int sideA, sideB, sideC;
+	private double perimeter;
+	private double theArea;
+
+	public Triangle()
+	{
+	   setSides(0,0,0);
+	   perimeter=0;
+	   theArea=0;
+	}
+
+	public Triangle(int a, int b, int c)
+	{
+
+
+	}
+
+
+	public void setSides(int a, int b, int c)
+	{
+
+
+	}
+
+	public void calcPerimeter( )
+	{
+
+
+	}
+
+	public void calcArea( )
+	{
+		double s;
+
+
+	}
+
+	public void print( )
+	{
+
+
+		System.out.println("\n\n");
+	}
+}
