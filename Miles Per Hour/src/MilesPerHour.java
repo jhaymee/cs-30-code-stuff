65066a48752c724bf3d2e71b0c042116227a21cf
jhaymee <jhaymee-belle.bisenio@rcsd.ca>
2024-05-01 12:05:29 -0600

    started mph

---
+++ Miles Per Hour/src/MilesPerHour.java
@@ -0,0 +1,46 @@
+// Program name: (The name you gave the program)
+
+// Student name: (Your name)
+
+// Date: (The date you completed the program)
+
+// Program Description:
+
+import java.util.Scanner; 
+import static java.lang.System.*;
+import static java.lang.Math.*;
+
+public class MilesPerHour
+{
+	private int distance, hours, minutes, time, rate;
+	private double mph;
+
+	public MilesPerHour() {
+		setNums(0,0,0);
+		mph=0.0;
+	}
+
+	public MilesPerHour(int dist, int hrs, int mins) {
+		distance = dist;
+		hours = hrs;
+		minutes = mins;
+
+	}
+
+	public void setNums(int dist, int hrs, int mins) {
+		distance = dist;
+		hours = hrs;
+		minutes = mins;
+
+	}
+
+	public void calcMPH() {
+
+
+	}
+
+	public void print() {
+
+
+	}
+}
