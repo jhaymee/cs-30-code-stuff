5fb5c19f7933ad35343623677790ed540483bfd0
jhaymee <jhaymee-belle.bisenio@rcsd.ca>
2024-05-07 12:07:27 -0600

    worked on cool numbers

---
+++ Cool Numbers/src/CoolNumbers.java
@@ -0,0 +1,36 @@
+// Program name: (The name you gave the program)
+
+// Student name: (Your name)
+
+// Date: (The date you completed the program)
+
+// Program Description:
+
+import static java.lang.System.*;
+
+public class CoolNumbers 
+{
+	/*
+	 *method isCoolNumber will return true if
+	 *	num % 3-6 all have a remainder of 1
+	 *it will return false otherwize
+	 */
+	public static boolean isCoolNumber( int num ) {
+		int x = 2 < x < 7;
+		if (num % x ) {
+			return true;
+		}
+		 return false;
+	}
+	
+		/*
+	 *method countCoolNumbers will return the count
+	 *of the coolNumbers between 6 and stop
+	 */
+	public static int countCoolNumbers( int stop ) {
+
+		return 0;
+	}
+}
+
+// for loop: for (i = 6; i <= num; i++);
