287eb2e1c603b581d1328641a0686809ba5d1674
jhaymee <jhaymee-belle.bisenio@rcsd.ca>
2024-05-10 12:11:30 -0600

    created loop stats

---
+++ Loop Stats/src/LoopStats.java
@@ -0,0 +1,60 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name
+
+import static java.lang.System.*;
+
+public class LoopStats
+{
+	private int start, stop;
+
+	public LoopStats()
+	{
+	}
+
+	public LoopStats(int beg, int end)
+	{
+	}
+
+	public void setNums(int beg, int end)
+	{
+
+
+	}
+
+	public int getEvenCount()
+	{
+		int evenCount=0;
+
+
+
+
+		return evenCount;
+	}
+
+	public int getOddCount()
+	{
+		int oddCount=0;
+
+
+
+
+
+		return oddCount;
+	}
+
+	public int getTotal()
+	{
+		int total=0;
+
+
+
+
+		return total;
+	}
+	
+	public String toString()
+	{
+		return start + " " + stop;
+	}
+}
